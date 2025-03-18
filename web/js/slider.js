78d0fcf3ca0f9b7a3798df5966f9cf19f959fda6
kao4 <167847955+kao4@users.noreply.github.com>
2025-03-18 17:01:02 +0900

    nextエントリの表示を出力

---
+++ web/js/slider.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // すべてのスライダーを取得
+  document.querySelectorAll(".splide").forEach(function (element) {
+    new Splide(element, {
+      type: "loop",
+      perPage: 2.5,
+      gap: "1.5rem",
+      padding: { left: "16rem", right: "16rem" },
+      arrows: false,
+      pagination: false,
+      autoplay: true,
+      speed: 1000,
+      breakpoints: {
+        1200: {
+          perPage: 2,
+          padding: { left: "4rem", right: "4rem" },
+        },
+        960: {
+          perPage: 1.5,
+          padding: { left: "4rem", right: "4rem" },
+        },
+      },
+      interval: 4000,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      resetProgress: false,
+    }).mount();
+  });
+});
