14abb210b769ac4167703395a83c396564ccbc0b
kao4 <167847955+kao4@users.noreply.github.com>
2025-03-26 14:25:16 +0900

    スライダーの設定を更新し、表示数を2.5に変更。CSSスタイルを調整し、スライドのオーバーフローを隠すように修正。レスポンシブデザインを強化。

--- web/js/slider.js
+++ web/js/slider.js
@@ -3,9 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".splide").forEach(function (element) {
     new Splide(element, {
       type: "loop",
-      perPage: 2,
+      perPage: 2.5,
+      perMove: 1 /* 1枚ずつスライド */,
+      start: 0,
       gap: "1.5rem",
-      padding: { left: "16rem", right: "16rem" },
       arrows: false,
       pagination: false,
       autoplay: true,
@@ -13,7 +14,6 @@ document.addEventListener("DOMContentLoaded", function () {
       breakpoints: {
         960: {
           perPage: 2,
-          padding: { left: "4rem", right: "4rem" },
         },
         // 960: {
         //   perPage: 1.5,
